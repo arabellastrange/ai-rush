75d571da6d1df981be97a22feb836c19a1d553e6
Chadha <shatha.degachi@gmail.com>
2018-01-29 15:27:36 +0000

    started depthfirst

--- src/rushhour/solver/BreadthFirstSearch.java
+++ src/rushhour/solver/BreadthFirstSearch.java
@@ -1,5 +1,6 @@
 package rushhour.solver;
 
+import java.util.List;
 import java.util.Stack;
 
 import rushhour.model.move.Move;
@@ -10,8 +11,18 @@ public class BreadthFirstSearch extends AbstractSolver {
 	@Override
 	public Stack<Move> solve(State state) {
 		start = System.currentTimeMillis();
+		List<State> children = state.getChildren();
 
-		return fail();
+		State current = children.get(0);
+
+        while(!current.isGoal()) {
+            nodeCount++;
+            children = current.getChildren();
+            State next = children.get(1);
+            current = next;
+        }
+
+        return calculateMoves(state, current);
 	}
 	
 }
--- src/rushhour/solver/DepthFirstSearch.java
+++ src/rushhour/solver/DepthFirstSearch.java
@@ -1,5 +1,7 @@
 package rushhour.solver;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 import rushhour.model.move.Move;
@@ -9,8 +11,31 @@ public class DepthFirstSearch extends AbstractSolver {
 
 	@Override
 	public Stack<Move> solve(State state) {
+	    Stack stack = new Stack();
+
 		start = System.currentTimeMillis();
-		return fail();
+
+		List<State> children = state.getChildren();
+		//List<State> visited = new ArrayList<State>() ;
+
+        State current = children.get(0);
+        stack.push(current);
+        //visited.add(current);
+
+
+		while(!current.isGoal() && !stack.isEmpty()) {
+			nodeCount++;
+			children = current.getChildren();
+			if(children.size() != 0){
+                State next = children.get(1);
+                current = next;
+                stack.push(current);
+                //visited.add(current);
+            }
+
+		}
+
+		return calculateMoves(state, current);
 	}
 	
 }
