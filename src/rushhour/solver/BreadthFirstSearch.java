97e6931028380e3358eb87080d19e3b59f93908c
Chadha <shatha.degachi@gmail.com>
2018-01-30 12:10:44 +0000

    breadthfirst working

--- src/rushhour/solver/BreadthFirstSearch.java
+++ src/rushhour/solver/BreadthFirstSearch.java
@@ -12,19 +12,17 @@ public class BreadthFirstSearch extends AbstractSolver {
 		start = System.currentTimeMillis();
 
         Queue<State> queue = new LinkedList<State>();
-		List<State> children = state.getChildren();
+		List<State> children = state.getChildren(); // placeholder value
 		List<State> visited = new ArrayList<State>();
 		State current = state;
 		queue.add(current);
 
-        System.out.println("Parent: " + current.toString());
 		nodeCount++;
 
         while(!queue.isEmpty()) {
             current = queue.remove();
             visited.add(current);
 
-            System.out.println("Current: " + current.toString());
             nodeCount++;
 
             if(current.isGoal()){
@@ -34,7 +32,7 @@ public class BreadthFirstSearch extends AbstractSolver {
             children = current.getChildren();
             if(children.size() != 0){
                 for(int i = 0; i  < children.size(); i++){
-                    if(!visited.contains(children.get(i))){
+                    if(!visited.contains(children.get(i)) && !queue.contains(children.get(i))){
                         queue.add(children.get(i));
                     }
                 }
